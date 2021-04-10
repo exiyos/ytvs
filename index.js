9c8bc0c9648dd34793cee3f62a6bb6cccaec1b95
Adil <adilzoldyck@gmail.com>
2021-04-10 13:43:33 +0100

    prjct

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.on('uncaughtException', function(err) {
-    console.log(err);
+  console.log(err);
 });
 
 
@@ -11,43 +11,42 @@ var title;
 app.use('/views', express.static('views'));
 app.use(cors());
 app.listen(4000, () => {
-    console.log('Server Workss !!! At port 4000');
+  console.log('Server Workss !!! At port 4000');
 });
 
 app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/download', (req,res) => {
-var URL = req.query.URL;
-
-
-
-ytdl.getInfo(URL).then(info => {
+app.get('/download', (req, res) => {
+  var URL = req.query.URL;
+  ytdl.getInfo(URL).then(info => {
     console.log(info.videoDetails.title);
-    title = info.videoDetails.title;
-    res.header('Content-Disposition', `attachment; filename=${info.videoDetails.title}.mp4`);
-ytdl(URL, {
-    format: 'mp4'
+
+    res.setHeader('Content-Disposition', `attachment; filename=${info.videoDetails.title}.mp4`);
+    ytdl(URL, {
+      filter:'videoandaudio',
+      format: `mp4`
     }).pipe(res);
 
-})
+  });
 
 
-
-    
-
-
-
- 
+});
 
 
 
 
+app.get('/mp3', (req, res) => {
 
+  ytdl.getInfo(req.query.URL).then(info => {
+    console.log(info.videoDetails.title);
+    res.setHeader('Content-Disposition', `attachment; filename=${info.videoDetails.title}.mp3`);
+    ytdl(req.query.URL, {
+      filter: 'audioonly',
+      format: 'mp3'
+    }).pipe(res);
 
-
-
-
+  });
 
 });
--- views/index.js
+++ views/index.js
@@ -1,17 +1,41 @@
 var convertBtn = document.querySelector('.download');
 var URLinput = document.querySelector('.URL-input');
+var mp3btn = document.querySelector('.mp3');
+var mp4btn = document.querySelector('.mp4');
 
 
+mp3btn.addEventListener('click', function(){
+  mp3btn.classList.add('active');
+  mp4btn.classList.remove("active");
+});
+mp4btn.addEventListener('click', function(){
+  mp4btn.classList.add('active');
+  mp3btn.classList.remove("active");
+});
+
+
+var x = 0;
+
+
+function mp4(){
+  x = 1;
+}
+function mp3(){
+  x = 2;
+}
 
 
 function matchYoutubeUrl() {
   var url = URLinput.value;
   var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-  if (url.match(p)) {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
-  } else {
+  if (!url.match(p)) {
     alert("Insert a valid URL");
+  } else if(x==0){
+    alert("select a format");
+  }else{
+    
+    sendURL(URLinput.value);
+    
   }
 }
 
@@ -19,7 +43,15 @@ function matchYoutubeUrl() {
 
 
 
-convertBtn.addEventListener('click', () => {});
+
 function sendURL(URL) {
+  if(x==1){
   window.location.href = `http://localhost:4000/download?URL=${URL}`;
-}
+  }
+  if(x==2){
+  window.location.href = `http://localhost:4000/mp3?URL=${URL}`;
+  }
+  }
+  
+    
+    
